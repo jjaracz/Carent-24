d3d737b8a5326aa03a3b1ebf94c7949e166c871f
Krzysztof Szostak <Krzysztof Szostak@192.168.11.12>
2016-04-02 00:04:17 +0200

    Tworzę logowanie. Obczajaj dokumentacje.

--- module/Application/src/Application/Model/User/Form/UserLoginForm.php
+++ module/Application/src/Application/Model/User/Form/UserLoginForm.php
@@ -5,27 +5,28 @@ namespace Application\Model\User\Form;
 use Zend\Form\Form;
 
 class UserLoginForm extends Form {
+
     public function __construct() {
         parent::__construct('UserLoginForm');
-        
+
         $this->add(array(
-           'name' => 'login',
-           'type' => 'text',
-           'attributes' => array(
-               'class' => '',// wprowadź sobie klase/y
-               'placeholder' => 'Login',
-           )
+            'name' => 'login',
+            'type' => 'text',
+            'attributes' => array(
+                'class' => '', // wprowadź sobie klase/y
+                'placeholder' => 'Login',
+            ),
         ));
-        
+
         $this->add(array(
-           'name' => 'password',
-           'type' => 'password',
-           'attributes' => array(
-               'class' => '', // wprowadź sobie klase/y
-               'placeholder' => 'Hasło'
-           )
+            'name' => 'password',
+            'type' => 'password',
+            'attributes' => array(
+                'class' => '', // wprowadź sobie klase/y
+                'placeholder' => 'Hasło'
+            )
         ));
-        
+
         $this->add(array(
             'name' => 'submit',
             'type' => 'Submit',
@@ -36,5 +37,5 @@ class UserLoginForm extends Form {
             )
         ));
     }
-}
 
+}
